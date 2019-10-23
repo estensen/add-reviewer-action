4fded589c8dc43720c82c9d688e045b0709fa76c
Håvard Anda Estensen <haavard.ae@gmail.com>
2019-10-23 15:44:00 +0200

    Add entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -e
+
+[ -z "${INPUT_GITHUB_TOKEN}" ] && {
+    echo "Set the GITHUB_TOKEN environment variable."
+    exit 1;
+};
+
+echo "Adding $1 as reviewer"
+[ -z "${INPUT_USERNAME}" ] && {
+    echo "Set the USERNAME environment variable."
+    exit 1;
+};
+
+
+AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
+API_HEADER="Content-Type: application/json"
+pull_number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
+
+curl --request POST \
+  --url "https://api.github.com/repos/$GITHUB_ACTOR/$GITHUB_REPOSITORY/pulls/$pull_number/requested_reviewers \
+  -H "{AUTH_HEADER}" \
+  -H "{API_HEADER}" \
+  -X POST \
+  -d "{\"reviewers\":[\"INPUT_USERNAME\"]}"
